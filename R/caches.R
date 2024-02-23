24db12acb9fafed17fb4a73cdcee34bf3f7f7b31
Dewey Dunnington <dewey@fishandwhistle.net>
2024-02-23 15:56:57 -0400

    maybe fix S3 problems

--- R/caches.R
+++ R/caches.R
@@ -67,13 +67,31 @@ internal_cache_environment <- new.env(parent = emptyenv())
 # create interal cache (max size 1000 by default)
 internal_cache <- environment_cache(internal_cache_environment, max_size = 1000)
 
-# get/set cached info methods
+
+#' Internal cache methods
+#'
+#' For internal use only.
+#'
+#' @param cache,url,data,... For internal use only
+#'
+#' @return Values for internal use
+#' @export
 set_cached <- function(cache, url, data, ...) UseMethod("set_cached")
+
+#' @rdname set_cached
+#' @export
 get_cached <- function(cache, url, ...) UseMethod("get_cached")
-clear_cache <- function(cache) UseMethod("clear_cache")
-cache_size <- function(cache) UseMethod("cache_size")
+
+#' @rdname set_cached
+#' @export
+clear_cache <- function(cache, ...) UseMethod("clear_cache")
+
+#' @rdname set_cached
+#' @export
+cache_size <- function(cache, ...) UseMethod("cache_size")
 
 # set for directory caches
+#' @export
 set_cached.file_cache <- function(cache, url, data, ext = ".cached", ...) {
   url_hash <- digest::digest(url)
   if(!dir.exists(cache$directory) && !is.null(data)) {
@@ -90,6 +108,7 @@ set_cached.file_cache <- function(cache, url, data, ext = ".cached", ...) {
 }
 
 # get for directory caches
+#' @export
 get_cached.file_cache <- function(cache, url, ext = ".cached", ...) {
   url_hash <- digest::digest(url)
   fname <- file.path(cache$directory, paste0(url_hash, ext))
@@ -101,19 +120,22 @@ get_cached.file_cache <- function(cache, url, ext = ".cached", ...) {
 }
 
 # clear cache for files
-clear_cache.file_cache <- function(cache, ext = ".cached") {
+#' @export
+clear_cache.file_cache <- function(cache, ext = ".cached", ...) {
   # removing all *.cached files (safer than remvoing the dir)
   unlink(list.files(cache$directory, pattern = paste0("*", ext),
                     recursive = FALSE, full.names = TRUE))
 }
 
 # cache size, directory caches
-cache_size.file_cache <- function(cache) {
+#' @export
+cache_size.file_cache <- function(cache, ...) {
   length(list.files(cache$directory))
 }
 
 
 # get/set for environment caches
+#' @export
 get_cached.environment_cache <- function(cache, url, ...) {
   url_hash <- digest::digest(url)
   if(exists(url_hash, where = cache$env)) {
@@ -124,31 +146,37 @@ get_cached.environment_cache <- function(cache, url, ...) {
 }
 
 # environment caches are slightly simpler
+#' @export
 set_cached.environment_cache <- function(cache, url, data, ...) {
   cache$env[[digest::digest(url)]] <- data
   if(cache_full(cache)) warning("Cache full: (n=", cache_size(cache),
                                 "). Use file_cache for larger cache sizes")
 }
 
-cache_size.environment_cache <- function(cache) {
+#' @export
+cache_size.environment_cache <- function(cache, ...) {
   length(cache$env)
 }
 
-clear_cache.environment_cache <- function(cache) {
+#' @export
+clear_cache.environment_cache <- function(cache, ...) {
   items <- as.list(names(cache$env))
   do.call(rm, c(items, list(envir = cache$env)))
 }
 
 # the null cache does nothing and is empty
+#' @export
 set_cached.null_cache <- function(cache, url, data, ...) {
   warning("Attempt to set_cache for the null_cache")
   invisible(NULL)
 }
 
+#' @export
 get_cached.null_cache <- function(cache, url, ...) {
   NULL
 }
 
-cache_size.null_cache <- function(cache) {
+#' @export
+cache_size.null_cache <- function(cache, ...) {
   0
 }
